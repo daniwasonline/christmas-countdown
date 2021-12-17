e11e2f8486af8e64d6d6c7d2729078dc06f366f3
Daniel Hyders <git@danny.works>
2021-12-16 20:29:33 -0800

    Sounds and replacement of gif

--- src/jsx/componentry/Background.jsx
+++ src/jsx/componentry/Background.jsx
@@ -1,9 +1,11 @@
+import campfire from "../../audio/campfire.mp3";
+
 export default function Background() {
     return (
         <div className="FireplaceBackground" style={{ overflow: "hidden" }}>
+            <audio src={campfire} autoPlay loop volu />
             <div style={{ overflow: "hidden", position: "absolute", top: 0, left: 0, height: "100vh", width: "100vw" }}>
                 <div className="FireplaceBlur" />
-                    <iframe title="fp" className="FireplaceBackgroundIF" src="https://www.youtube.com/embed/iGwawktAMRg?loop=1&autoplay=1&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&fs=0&color=white&controls=0&disablekb=1&mute=1" frameborder="0" width="100%" height="100%" />
             </div>
         </div>
     );
